1c0993014089a3bc55fa3d5ea6de0965e16c145c
Raymond DeVries <raymond.l.devries@gmail.com>
2024-11-27 16:54:08 -0800

    Add basic function for netlify slackbot

---
+++ slackBot/go.mod
@@ -0,0 +1,8 @@
+module slackBot
+
+go 1.23.2
+
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/davecgh/go-spew v1.1.1
+)
---
+++ slackBot/netlify/functions/hello/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/davecgh/go-spew/spew"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	spew.Dump(request)
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, World!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
